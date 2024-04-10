4f9cb86bc9fd5da2a6ba9335fc193c261d0db84e
Sarahkiss215 <skisiangani5@gmail.com>
2024-04-10 04:41:20 -0700

    Add space lines in the code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -11,18 +11,22 @@ export default class HolbertonCourse {
     this.length = length;
     this.students = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
+
   get length() {
     return this._length;
   }
+
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
